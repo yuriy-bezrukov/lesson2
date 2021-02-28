1fa5f248a8d1db3e52cfee30870253b475849f2c
bezrukov <bezrukov-yra@mail.ru>
2021-02-28 17:43:39 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+humans = [
+  {name: 'vvv', lastname: 'rrr'},
+  {name: 'sdf', lastname: 'sgrg'},
+  {name: '23423', lastname: 'f34r34'},
+];
+
+// for -> vvv rrr
+
+// vvv rrr
+// sdf sgrg
+// 23423 f34r34
+
+// <- concat
+// a + ' ' + b 
+
+a = 96; // u -> 96
+a++; // 96 -> 97;
+b = ++a + 10; // 98 + 10 => b = 107 
+ // a == 98;
+
+
+ 1, 2, 3, 5, 8, 13, 21
+
+
+ lastNumber = 0;
+ firstNumber = 1;
+ for(let i = 0; i < 10; i++) {
+  console.log(/* */);
+  /** */
+}
